9046a5ca1b3d2b6f39ea32225739160f8b11a1a6
MohamedGira <mohamedgira0901@gmail.com>
2023-07-30 20:29:17 +0300

    separated collision detection logic

---
+++ CollisionDetector.js
@@ -0,0 +1,30 @@
+export class CollisionDetector {
+  static detectCollesion(a, b, event) {
+    /*
+    detects collision between 2 Drawables, emmits "event" on collision 
+    */
+    let aXStart, aXEnd, aYStart, aYEnd, bXStart, bXEnd, bYStart, bYEnd;
+
+  
+    setInterval(() => {
+      aXStart = a.px + (a.sequence.whiteSpaceX * a.scale) / 2;
+      aXEnd = aXStart + a.physicalWidth;
+      aYStart = a.py + (a.sequence.whiteSpaceY * a.scale) / 2;
+      aYEnd = aYStart + a.physicalHeight;
+
+      bXStart = b.px + (b.sequence.whiteSpaceX * b.scale) / 2;
+      bXEnd = bXStart + b.physicalWidth;
+      bYStart = b.py + (b.sequence.whiteSpaceY * b.scale) / 2;
+      bYEnd = bYStart + b.physicalHeight;
+      if (
+        aXStart > bXEnd || // x: a begin after my end
+        aXEnd < bXStart || // x: a end befire my begin
+        aYStart > bYEnd || // y: a begin after my end
+        aYEnd < bYStart // y: a end befire my begin
+      ) {
+      } else {
+        document.dispatchEvent(event);
+      }
+    }, 40);
+  }
+}
