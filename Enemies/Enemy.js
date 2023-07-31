0a7f4a20d0ea036920b5d88199cba3396515e358
MohamedGira <mohamedgira0901@gmail.com>
2023-07-31 18:21:31 +0300

    Moved to separate folder

--- Enemies/Enemy.js
+++ Enemies/Enemy.js
@@ -28,52 +28,12 @@ export class Enemy extends Drawable {
     CollisionDetector.detectCollesion(this, foe, this.collisionEvent);
     this.exploded=false;
   }
-  
-  isoutOfScreen(){
-    return (this.px < -this.sequence.frameWidth || this.py < -this.sequence.frameHeight || this.px > CANVAS_WIDTH+this.sequence.frameWidth || this.py > CANVAS_HEIGHT+this.sequence.frameHeight)
-  }
+
 
   update() {
     this.outOfScreen=this.isoutOfScreen();
     super.update();
   }
-  setpositionX(x) {
-    this.px = x;
-  }
-  setpositionY(y) {
-    this.py = y;
-  }
-  changeVelocity(dx = 0, dy = 0) {
-    this.vx += dx;
-    this.vy += dy;
-  }
-  setVelocityX(dx) {
-    this.vx = dx;
-  }
-  setVelocityY(dy) {
-    this.vy = dy;
-  }
-  noiseMoveX(strengthx = 1) {
-    this.changeVelocity(Math.random() * strengthx - strengthx / 2, 0);
-  }
-  noiseMoveY(strengthy = 1) {
-    this.changeVelocity(0, Math.random() * strengthy - strengthy / 2);
-  }
-  oscillateX(amplitude = 1, speedMs = 50) {
-    let i = 0;
-    setInterval(() => {
-      this.setVelocityX(Math.sin((i++ * 3.14) / 360) * amplitude);
-      this.noiseMoveX(amplitude * 0.6);
-    }, speedMs);
-  }
-  oscillateY(amplitude = 1, speedMs = 1) {
-    let i = 0;
-    setInterval(() => {
-      this.setVelocityY(Math.sin((i++ * 180) / 3.14) * amplitude);
-      this.noiseMoveY(amplitude);
-    }, speedMs);
-  }
-  static createEnemy() {}
 }
 
 
--- Enemies/Worm.js
+++ Enemies/Worm.js
@@ -5,15 +5,7 @@ import { Enemy } from "./Enemy.js";
 export class Worm extends Enemy {
   static sequence = getSequence("../assets/enemies/enemy_worm.png",80,60,6,0);
   constructor(canvas, animationSpeed, renderAtX, renderAtY, scale, foe) {
-    super(
-      canvas,
-      Worm.sequence,
-      animationSpeed,
-      renderAtX,
-      renderAtY,
-      scale,
-      foe,{score:5}
-    );
+    super(canvas,Worm.sequence,animationSpeed,renderAtX,renderAtY,scale,foe,{score:5});
     this.vx = animationSpeed*-3;
     this.py= ground- this.sequence.frameHeight*this.scale;
     this.px= CANVAS_WIDTH;
