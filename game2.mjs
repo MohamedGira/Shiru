886753aa59b1f4934f5ac431bd7e0d5c6f35cc89
MohamedGira <mohamedgira0901@gmail.com>
2023-08-03 03:13:22 +0300

    merged

--- game2.mjs
+++ game2.mjs
@@ -8,109 +8,21 @@ import { Worm } from "./Enemies/Worm.js";
 import { Trail } from "./Trail.js";
 import { showMessage } from "./utils/showMessage.js";
 import { Heart } from "./Heart.js";
+import { InputHandler } from "./Dog/InputHandler.js";
+import { states } from "./Dog/States/State.js";
+import { Dog } from "./Dog/Dog.js";
 
 const canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 const timeInterval = 1000;
 let EnemyAs = [];
-
+let music = new Audio("./assets/sounds/the_field_of_dreams.mp3");
+music.loop = true;
 var myFont = new FontFace("Pixels", "url(assets/VT323/VT323-Regular.ttf)");
 
 myFont.load().then(function (font) {
   document.fonts.add(font);
 });
-
-let music = new Audio("./assets/sounds/the_field_of_dreams.mp3");
-export let play=false;
-document.getElementById('play').addEventListener("click", () => {
-  play=true
-  music.volume=.1
-  music.play();
-  animate(0);
-  !continueAnimating&&location.reload()
-});
-
-music.loop = true;
-setInterval(() => {
-  EnemyAs.push(
-    new Ghost(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      0.3,
-      puppy
-    )
-  );
-  EnemyAs.push(
-    new EnemyA(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      0.3,
-      puppy
-    )
-  );
-}, 1200);
-setInterval(() => {
-  EnemyAs.push(
-    new EnemyD(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      0.3,
-      puppy
-    )
-  );
-  EnemyAs.push(
-    new Worm(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      2,
-      puppy
-    )
-  );
-}, 5000);
-
-setInterval(() => {
-  EnemyAs.push(
-    new EnemyD(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      0.3,
-      puppy
-    )
-  );
-  EnemyAs.push(
-    new Heart(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      1,
-      puppy
-    )
-  );
-}, 5000);
-
-setInterval(() => {
-  EnemyAs.push(
-    new EnemyB(
-      ctx,
-      Math.random(),
-      Math.random() * CANVAS_WIDTH,
-      Math.random() * CANVAS_HEIGHT,
-      0.3,
-      puppy
-    )
-  );
-}, 400);
 
 let bgs = getBgs(ctx);
 
@@ -124,11 +36,145 @@ for (let i = 0; i < 1000; i++) {
   });
   trailsPool.push({ isActive: false, object: new Trail(ctx, 0.2, 0, 0, 0.2) });
 }
+function handleTrails(forDrawable) {
+  let trail = trailsPool[trailsPool.findIndex((el) => !el.isActive)];
+  if (trail) {
+    trail.object.setPosition(
+      forDrawable.px + forDrawable.physicalWidth / 2 + Math.random() * 50 - 25,
+      forDrawable.py + forDrawable.physicalHeight / 2 + Math.random() * 50 - 25
+    );
+    trail.object.setVelocity(-forDrawable.vx * 0.2, -forDrawable.vy * 0.2);
+    trail.object.setAcceleration(0, -0.1);
+    trail.object.setScale(Math.random() * 1.4);
+    trail.isActive = true;
+  }
+}
+
+const inputHandler = new InputHandler();
 
 let score = 0;
 let passed = 0,
   lastTime = 0;
 let continueAnimating = true;
+
+let play = document.getElementById("play");
+let puppy;
+
+let EnemyGen1, EnemyGen2, EnemyGen3, EnemyGen4;
+play.addEventListener("click", () => {
+  play.style.display = "none";
+  music.play();
+  clearInterval(EnemyGen1);
+  clearInterval(EnemyGen2);
+  clearInterval(EnemyGen3);
+  clearInterval(EnemyGen4);
+  continueAnimating = true;
+  puppy = new Dog(ctx, 0.4, 0, 250, 0.3, { initialLives: 10 });
+  animate(0);
+  EnemyAs = [];
+  EnemyGen1 = setInterval(() => {
+    EnemyAs.push(
+      new Ghost(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        Math.random() * 0.5 + 0.3,
+        puppy
+      )
+    );
+    EnemyAs.push(
+      new EnemyA(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        Math.random() * 0.3 + 0.3,
+        puppy
+      )
+    );
+  }, 1200);
+  EnemyGen2 = setInterval(() => {
+    EnemyAs.push(
+      new EnemyD(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        Math.random() * 0.5 + 0.3,
+        puppy
+      )
+    );
+    EnemyAs.push(
+      new Worm(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        Math.random() + 2,
+        puppy
+      )
+    );
+  }, 5000);
+
+  EnemyGen3 = setInterval(() => {
+    EnemyAs.push(
+      new Heart(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        1,
+        puppy
+      )
+    );
+  }, 3000);
+
+  EnemyGen4 = setInterval(() => {
+    EnemyAs.push(
+      new EnemyB(
+        ctx,
+        Math.random(),
+        Math.random() * CANVAS_WIDTH,
+        Math.random() * CANVAS_HEIGHT,
+        Math.random() * 0.3 + 0.3,
+        puppy
+      )
+    );
+  }, 400);
+
+  document.addEventListener("collision", function (e) {
+    let en = e.detail.whoami;
+    if (en && puppy.currentStateIndex != states.DYING) {
+      let ex = boomsPool[boomsPool.findIndex((el) => !el.isActive)];
+      ex.object.setPosition(en.px, en.py);
+      ex.object.setScale(
+        (en.physicalWidth * 1.2) / ex.object.sequence.frameWidth
+      );
+      ex.isActive = true;
+      EnemyAs = EnemyAs.filter((el) => el != en);
+      if (en instanceof Enemy) {
+        if (puppy.currentStateIndex == states.ROLLING)
+          score += en.options.score;
+        else {
+          puppy.lives.decrementLives();
+        }
+      } else if (en instanceof Heart) {
+        puppy.lives.incrementLives();
+      }
+      delete e.detail.whoami;
+    }
+  });
+});
+let hide = true;
+function hideHead() {
+  hide &&
+    document.getElementById("head").animate([{ opacity: 1 }, { opacity: 0 }], {
+      duration: 1000,
+      fill: "forwards",
+    });
+  hide = false;
+}
 
 function animate(timeStamp) {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -154,24 +200,20 @@ function animate(timeStamp) {
   if (passed > timeInterval) {
     EnemyAs = EnemyAs.filter((enemy) => !enemy.outOfScreen);
   }
-  puppy.animate();
-  if (puppy.state == "roll") {
-    let trail = trailsPool[trailsPool.findIndex((el) => !el.isActive)];
-    if (trail) {
-      trail.object.setPosition(
-        puppy.px + puppy.physicalWidth / 2 + Math.random() * 50 - 25,
-        puppy.py + puppy.physicalHeight / 2 + Math.random() * 50 - 25
-      );
-      trail.object.setVelocity(-puppy.vx * 0.2, -puppy.vy * 0.2);
-      trail.object.setAcceleration(0, -0.1);
-
-      trail.object.setScale(Math.random() * 1.4);
-      trail.isActive = true;
-    }
+  puppy.draw();
+  puppy.update(inputHandler.lastKey, inputHandler.isPress);
+  if (puppy.currentStateIndex == states.ROLLING) {
+    handleTrails(puppy);
   }
   puppy.lives.draw();
-  if (puppy.state == "die") {
-    puppy.ax = 0;
+  if (puppy.currentStateIndex == states.DYING) {
+    hide && canvas.animate([{ opacity: 1 }, { opacity: 0 }], 3000);
+    const fadeout=hide&&setInterval(()=>{
+      music.volume*=0.9;
+      if (music.volume<0.1)
+      clearInterval(fadeout);
+    },200)
+    hide = false;
     showMessage(ctx, `Game Over`, {
       font: "100px Pixels",
       renderAtX: CANVAS_WIDTH / 2 - 200,
@@ -180,49 +222,31 @@ function animate(timeStamp) {
       color: "red",
       shadowColor: "black",
     });
-    document.getElementById('play').innerHTML='Try Again'
-  }
-  if (puppy.state == "die" && puppy.isGrounded) {
-    puppy.vx = -4;
-  }
-  if (
-    puppy.state == "die" &&
-    Math.floor(puppy.index * puppy.animationSpeed) ==
-      puppy.sequence.frames.length - 1
-  ) {
-    puppy.update = () => {};
+    
+    setTimeout(() => {
+      continueAnimating = false;
+      document.getElementById("btnImg").src = "./assets/tryAgain.png";
+      play.style.display = "block";
+      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }, 3000);
   }
   continueAnimating && requestAnimationFrame(animate);
 }
-//Collision
 
-document.addEventListener("collision", function (e) {
-  let en = e.detail.whoami;
-  if (en && puppy.state != "die") {
-    let ex = boomsPool[boomsPool.findIndex((el) => !el.isActive)];
-    ex.object.setPosition(en.px, en.py);
-    ex.object.setScale(
-      (en.physicalWidth * 1.2) / ex.object.sequence.frameWidth
-    );
-    ex.isActive = true;
-    EnemyAs = EnemyAs.filter((el) => el != en);
-    if (en instanceof Enemy) {
-      if (puppy.state == "roll") score += en.options.score;
-      else {
-        puppy.lives.decrementLives();
-        if (puppy.lives.lives <= 0) {
-          puppy.index = 0;
-          puppy.animationSpeed = 0.18;
-          puppy.state = "die";
-          let i = 0;
-          setTimeout(() => {
-            continueAnimating = false;
-          }, 3000);
-        }
-      }
-    } else if (en instanceof Heart) {
-      puppy.lives.incrementLives();
-    }
-    delete e.detail.whoami;
+let controls=document.getElementById('controlsList');
+document.getElementById('controls').addEventListener('mouseover',()=>{
+controls.style.display='flex';
+controls.style.position='fixed';
+controls.style.left='10px';
+controls.style.maxWidth='20rem';
+
+
+})
+document.getElementById('controls').addEventListener('mouseout',()=>{
+  controls.style.display='none';
+})
+window.addEventListener('keydown', function(e) {
+  if(e.code == 'Space' && e.target == document.body) {
+    e.preventDefault();
   }
 });
