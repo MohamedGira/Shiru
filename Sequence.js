8d529ffecd96f2e37c969c09f5f23262e8a98361
MohamedGira <mohamedgira0901@gmail.com>
2023-08-04 18:44:15 +0300

    new test for img size

--- Sequence.js
+++ Sequence.js
@@ -1,11 +1,19 @@
 class Sequence {
-  constructor(image,frameWidth, frameHeight, framesCount, offset,whiteSpaceXPercent,whiteSpaceYPercent) {
-    /* */
-    this.frameWidth = frameWidth;
-    this.frameHeight = frameHeight;
+  constructor(
+    image,
+    frameWidth,
+    frameHeight,
+    framesCount,
+    offset,
+    whiteSpaceXPercent,
+    whiteSpaceYPercent
+  ) {
+    this.CompressScale=.2
+    this.frameWidth = frameWidth*this.CompressScale;
+    this.frameHeight = frameHeight*this.CompressScale;
     this.framesCount = framesCount;
-    this.whiteSpaceX=(whiteSpaceXPercent||0)*this.frameWidth;
-    this.whiteSpaceY=(whiteSpaceYPercent||0)*this.frameHeight;
+    this.whiteSpaceX = (whiteSpaceXPercent || 0) * this.frameWidth;
+    this.whiteSpaceY = (whiteSpaceYPercent || 0) * this.frameHeight;
     this.image = image;
     this.offset = offset;
     this.frames = [];
@@ -15,6 +23,15 @@ class Sequence {
         y: offset * this.frameHeight,
       });
     }
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const smallWidth = this.image.width*this.CompressScale;
+    const smallHeight = this.image.height*this.CompressScale;
+    canvas.width = smallWidth;
+    canvas.height = smallHeight;
+    context.drawImage(this.image, 0, 0, smallWidth, smallHeight);
+    this.image = new Image();
+    this.image.src = canvas.toDataURL();
   }
 }
 export default Sequence;
