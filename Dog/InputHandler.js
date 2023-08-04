2e389ca737a5108a5bd4c65fccc7bf122dbb66bd
MohamedGira <mohamedgira0901@gmail.com>
2023-08-04 18:43:44 +0300

    added touch effects

--- Dog/InputHandler.js
+++ Dog/InputHandler.js
@@ -3,6 +3,10 @@ export class InputHandler {
     this.lastKey = null;
     this.isPress = undefined;
     this.activeKeys = new Set();
+    this.touchStart = null;
+    this.touchCurrent = null;
+    this.touchY = 0;
+    this.touchThreshold = 30;
     this.acceptedKeys = [
       "KeyW",
       "ArrowUp",
@@ -16,7 +20,6 @@ export class InputHandler {
       "ShiftRight",
       "ShiftLeft",
       "KeyR",
-      
     ];
     window.addEventListener("keydown", (event) => {
       if (this.acceptedKeys.includes(event.code)) {
@@ -36,6 +39,55 @@ export class InputHandler {
         }
       }
     });
+    window.addEventListener("touchstart", (event) => {
+      this.touchStart = {
+        x: event.changedTouches[0].clientX,
+        y: event.changedTouches[0].clientY,
+      };
+    });
+    window.addEventListener("touchmove", (event) => {
+    
+      
+      this.handleSwipe(
+        "up",
+        this.touchStart.y - event.changedTouches[0].clientY > this.touchThreshold
+      );
+      this.handleSwipe(
+        "right",
+        event.changedTouches[0].clientX - this.touchStart.x > this.touchThreshold
+      );
+
+      this.handleSwipe(
+        "left",
+        this.touchStart.x - event.changedTouches[0].clientX > this.touchThreshold
+      );
+
+      this.touchCurrent = {
+        x: event.changedTouches[0].clientX,
+        y: event.changedTouches[0].clientY,
+      };
+      console.log(this.activeKeys)
+    });
+    window.addEventListener("touchend", (event) => {
+      this.touchStart = null;
+      this.touchCurrent = null;
+      this.handleSwipe("up", this.touchCurrent);
+      this.handleSwipe("right", this.touchCurrent);
+      this.handleSwipe("left", this.touchCurrent);
+    });
+  }
+  handleSwipe(movename, condition) {
+    if (condition) {
+      this.lastKey = movename;
+      this.activeKeys.add(this.lastKey);
+      this.isPress = true;
+    } else {
+      this.activeKeys.delete(movename);
+      this.lastKey = this.activeKeys.values().next().value;
+      if (!this.lastKey) {
+        this.isPress = false;
+      }
+    }
   }
   whatKey(event) {
     switch (event.code) {
