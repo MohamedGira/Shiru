cd21b852d7216e7dbbdc786ed500a52993e772ad
MohamedGira <mohamedgira0901@gmail.com>
2023-08-04 19:32:15 +0300

    compression conditions

--- Sequence.js
+++ Sequence.js
@@ -8,13 +8,18 @@ class Sequence {
     whiteSpaceXPercent,
     whiteSpaceYPercent
   ) {
-    this.compressionScale=.2
     this.frameWidth = frameWidth;
     this.frameHeight = frameHeight;
     this.framesCount = framesCount;
     this.whiteSpaceX = (whiteSpaceXPercent || 0) * this.frameWidth;
     this.whiteSpaceY = (whiteSpaceYPercent || 0) * this.frameHeight;
     this.image = image;
+    this.compressionScale=1
+    if(this.image.width>1000||this.image.height>1000){
+      let max=Math.max(this.image.width,this.image.height)
+      this.compressionScale=1000/max
+    }
+
     this.offset = offset;
     this.compressedframeHeight = this.frameHeight*this.compressionScale;
     this.compressedframeWidth = this.frameWidth*this.compressionScale;
