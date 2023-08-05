25b4d71a9445b53f00606f9bd089fdd87d63970c
MohamedGira <mohamedgira0901@gmail.com>
2023-08-05 13:01:40 +0300

    tuning

--- Dog/Touchpad.js
+++ Dog/Touchpad.js
@@ -43,13 +43,14 @@ export class TouchPad {
   draw() {
     this.ctx.beginPath();
     this.ctx.arc(this.px, this.py, this.outerRadius, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "rgba(0,0,0,.2)";
+    this.ctx.fillStyle = "rgba(255,255,255,.7)";
     this.ctx.strokeStyle = "rgba(0,0,0,1)";
     this.ctx.fill();
     this.ctx.beginPath();
     this.ctx.arc(this.px, this.py, this.midRadus, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "rgba(0,0,0,.2)";
-    this.ctx.strokeStyle = "rgba(0,0,0,1)";
+    this.ctx.fillStyle = "rgba(0,0,0,.4)";
+    this.ctx.strokeStyle = "rgba(0,0,0,.2)";
+    this.ctx.stroke()
     this.ctx.fill();
 
     this.ctx.beginPath();
